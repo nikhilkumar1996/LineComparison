bd8a96dde5b44c959cc4c2f9475ec566b5800326
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-03 23:32:57 +0530

    [Nikhil Kumar]:Lines are Compared (eq,le,ge)

--- LineComparison/Geometry.cs
+++ LineComparison/Geometry.cs
@@ -8,20 +8,44 @@ namespace LineComparison
 {
     public class Geometry
     {
-        
         int x1, y1, x2, y2;
-        public Geometry(int CorX1, int CorY1, int CorX2, int CorY2)
+        string line1, line2;
+        public Geometry(int CorX1, int CorX2, int CorY1, int CorY2, string line1, string line2)
         {
-            this.x1 = CorX1; 
+            this.x1 = CorX1;
             this.x2 = CorX2;
             this.y1 = CorY1;
             this.y2 = CorY2;
+            this.line1 = line1;
+            this.line2 = line2;
         }
-        public double Coordinates()
+        public double Coordinates(string line1, string line2)
         {
-            double length = Math.Sqrt(Math.Pow((x1 - x2), 2)) + Math.Sqrt(Math.Pow((y2 - y1), 2));
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2)) + Math.Sqrt(Math.Pow((y2 - y1), 2));
             return length;
+
+
+        }
+
+        public void ComparingLines()
+        {
+            if (line1 == line2)
+            {
+                Console.WriteLine("Line1 is equal to line2");
+
+            }
+            if (line1.CompareTo(line2) > 0)
+            {
+
+                Console.WriteLine("Line1 is greater than line2");
+
+            }
+            else
+            {
+                Console.WriteLine("Line1 is less than line2");
+
+            }
+
         }
     }
-
 }
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,19 +6,11 @@ namespace LineComparison
         public static void Main()
         {
             Console.WriteLine("Welcome to Line Comparison Program");
-            Geometry geo = new Geometry(4, 3, 6, 2);
-            double line1 = geo.Coordinates();
-            Geometry geo1 = new Geometry(2, 1, 4, 3);
-            double line2 = geo1.Coordinates();
-            if (line1 == line2)
-            {
-                Console.WriteLine("Line1 is equal to line2");
-                Console.WriteLine("Line1=" + line1 + " " + "Line2=" + line2);
-            }
-            else
-            {
-                Console.WriteLine("Lines are different");
-            }
+
+            Geometry geo = new Geometry(4, 3, 6, 2, "Line1", "Line2");
+            geo.Coordinates("Line1", "Line2");
+            geo.ComparingLines();
+
         }
     }
 }
