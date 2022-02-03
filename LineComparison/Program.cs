32e201c1e28718414cf92d1b829a3308fe0ac954
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-03 23:13:50 +0530

    [Nikhil Kumar]:Welcome to Line Comparison program

---
+++ LineComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LineComparison
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Line Comparison Program");
+        }
+    }
+}
+
